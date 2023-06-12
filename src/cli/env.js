982282180624d57b488462c74e05f4cac433c008
Dzmitry Halavach <kotik1@protonmail.com>
2023-06-12 23:39:37 +0300

    feat: cli task

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,13 @@
 const parseEnv = () => {
-    // Write your code here 
+  // Loop over the keys of process.env
+  for (let key of Object.keys(process.env)) {
+    // Check if the key starts with RSS_
+    if (key.startsWith('RSS_')) {
+      // Print the key and value to the console
+      console.log(`${key} = ${process.env[key]}`);
+    }
+  }
+  //console.log(process.env);
 };
 
 parseEnv();
