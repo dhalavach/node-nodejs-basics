27e81d8a0c88ca27ba8755041c26ab989650d1df
Dzmitry Halavach <kotik1@protonmail.com>
2023-06-16 21:06:09 +0300

    feat: child process task

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,17 @@
+import { fork } from 'child_process';
+
+const SCRIPT_URL = new URL('./files/script.js', import.meta.url);
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  const child = fork(SCRIPT_URL, args, { silent: true });
+
+  process.stdin.pipe(child.stdin);
+  child.stdout.pipe(process.stdout);
+
+  child.stdout.on('data', (chunk) => {
+    process.stdout.write(`Received from child process: ${chunk}`);
+  });
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(/* [someArgument1, someArgument2, ...] */);
