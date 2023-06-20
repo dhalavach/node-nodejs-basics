1463eed0e619fa8e2bf2f51426bd4ca418695449
Dzmitry Halavach <kotik1@protonmail.com>
2023-06-20 14:27:05 +0300

    fix: delete file task

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,6 +1,6 @@
 import { rm } from 'fs/promises';
 import { join, resolve } from 'path';
-import { join } from 'path';
+import path from 'path';
 import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
